6d87f32b56ce853ef27fc339b7ad5cda5e7cea0f
Shubham Agarwal <shubham2002jindal@gmail.com>
2022-10-07 16:02:31 +0530

    create sign up page and sign in form

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,4 +1,9 @@
+const { response } = require("express");
+
 module.exports.home = function(req,res){
+    console.log(req.cookies);
+    res.cookie('user_id',45);
+    res.cookie('Something',7851064541);
     return res.render('home',{
         title : "Home"
     });
