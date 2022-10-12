641e141fdb706f7af42fe37f68f2ed394037d53b
ShubhamAgarwal-11 <shubham2002jindal@gmail.com>
2022-10-12 16:43:12 +0530

    Complete authentication part using passport.js

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -7,12 +7,19 @@ module.exports.profile = function(req,res){
 }
 // render the sign up page
 module.exports.signUp = function(req,res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_up',{
         title : 'Codeial | Sign Up'
     });
 }
 // render the sign in page
 module.exports.signIn = function(req,res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     return res.render('user_sign_in',{
         title : 'Codeial | Sign In'
     });
@@ -40,5 +47,11 @@ module.exports.create = function(req,res){
 }
 // sign in and create a session for the user
 module.exports.createSession = function(req,res){
-    //todo later
+    return res.redirect('/');
+}
+module.exports.DestroySession = function(req,res){
+    req.logout(function(err){
+        if(err){ return next(err); }
+        res.redirect('/');
+    });
 }
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const userController = require('../controller/users_controller');
 
-router.get('/profile', userController.profile);
+router.get('/profile', passport.checkAuthentication , userController.profile);
 
 router.get('/sign-in',userController.signIn);
 router.get('/sign-up',userController.signUp);
 
 router.post('/create',userController.create);
+
+// use passport as a middleware to authenticate
+router.post('/create-session', passport.authenticate(
+    'local',
+    {failureRedirect : 'sign-in'}
+) , userController.createSession);
+
+router.get('/sign-out',userController.DestroySession);
 module.exports = router;
