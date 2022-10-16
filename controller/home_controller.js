8197851e61ed996a9f5ca4b9b94c4283c237d499
ShubhamAgarwal-11 <shubham2002jindal@gmail.com>
2022-10-16 14:29:38 +0530

    create posts

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,11 +1,22 @@
+const Post = require('../models/post');
 const { response } = require("express");
 
 module.exports.home = function(req,res){
-    console.log(req.cookies);
-    res.cookie('user_id',45);
-    res.cookie('Something',7851064541);
-    return res.render('home',{
-        title : "Home Page"
+    // console.log(req.cookies);
+    // res.cookie('user_id',45);
+    // res.cookie('Something',7851064541);
+    // Post.find({},function(err,posts){
+    //     return res.render('home',{
+    //         title : "Codeial | Home",
+    //         posts : posts
+    //     });
+    // });
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title : "Codeial | Home",
+            posts : posts
+        });
     });
 }
 
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,5 @@ const homeController = require("../controller/home_controller");
 
 router.get('/',homeController.home);
 router.use('/users',require('./users'));
-
+router.use('/posts',require('./posts'));
 module.exports = router;
