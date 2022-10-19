2898ca829a6561010650b722f50089c242c74317
ShubhamAgarwal-11 <shubham2002jindal@gmail.com>
2022-10-19 23:23:29 +0530

    create comment section and display comments

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 const homeController = require("../controller/home_controller");
+const { route } = require('./users');
 
 router.get('/',homeController.home);
 router.use('/users',require('./users'));
 router.use('/posts',require('./posts'));
+router.use('/comments',require('./comments'));
 module.exports = router;
