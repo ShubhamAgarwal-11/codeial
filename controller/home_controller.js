e26f1cfeab03d086cb0621c2368d8ba90c617b3e
ShubhamAgarwal-11 <shubham2002jindal@gmail.com>
2022-10-21 14:56:19 +0530

    profile links and update user account

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,6 +1,6 @@
 const Post = require('../models/post');
 const { response } = require("express");
-
+const User = require('../models/user');
 module.exports.home = function(req,res){
     // console.log(req.cookies);
     // res.cookie('user_id',45);
@@ -21,9 +21,12 @@ module.exports.home = function(req,res){
         }
     })
     .exec(function(err,posts){
-        return res.render('home',{
-            title : "Codeial | Home",
-            posts : posts
+        User.find({},function(err,users){
+            return res.render('home',{
+                title : "Codeial | Home",
+                posts : posts,
+                all_users: users
+            });
         });
     });
 }
